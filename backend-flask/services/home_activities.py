1c5304df90b681874adfe39c110e77258d32a097
Suril Dalal <surildalal23@gmail.com>
2023-03-03 01:11:57 +0000

    week2: implement cloudWatch logs

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,7 +5,7 @@ tracer = trace.get_tracer("home.activities.here")
 
 class HomeActivities:
   def run():
-
+    #logger.info("HomeActivities")
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
